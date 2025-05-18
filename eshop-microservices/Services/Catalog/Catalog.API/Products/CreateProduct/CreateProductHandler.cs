d288fb699a33a992b56ce20079ef11eab6346ec5
Eslam Eid <eeid@itecorp.net>
2025-05-18 12:18:14 +0300

    Validation Pipeline

--- eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,11 +1,24 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
-        //why we are directly injecting AI document session in here, and why we don't cover this session in the repository folder or any data folder ?
-        //Because AI document session object is already an abstraction of the database operations.So we don't need any additional abstractions or unnecessary code like repository patterns.
+    //why we are directly injecting AI document session in here, and why we don't cover this session in the repository folder or any data folder ?
+    //Because AI document session object is already an abstraction of the database operations.So we don't need any additional abstractions or unnecessary code like repository patterns.
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
     internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
