59b9d4ded43d0c6f97d33ce0232bf4eaf6cad3ef
Eslam Eid <eeid@itecorp.net>
2025-05-22 13:51:19 +0300

    basket

---
+++ Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidation : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidation()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("cart can't be null");
+        RuleFor(x => x.Cart.UserName).NotNull().WithMessage("username is required");
+    }
+}
+
+public class StoreBasketCommandHandler :
+    ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+        //TODO : store basket in database (use martin upsert - if existing data = update -
+        //If not this will be insert any record and after that we have to update the cache in Redis distributed)
+        return new StoreBasketResult("swn");
+    }
+}
---
+++ Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,33 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+//By this way we can add Carter related classes into our container.
+builder.Services.AddCarter();
+
+//in here we can configure our mediator injection with specifying additional details(By this way we can register our mediator into current assembly.).
+var assembly = typeof(Program).Assembly;
+//configure Martin with our PostgreSQL connection string.specify the shopping cart entity will use the username property as its identity field.
+//And lastly, use lightweight sessions to optimize performance by utilizing Martin's lightweight
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x=>x.UserName);
+}).UseLightweightSessions();
+// Add service to the container.
+
+var app = builder.Build();
+
+// Configure the http request pipeline .
+
+//And this will be mapped Carter endpoints into our ASP.Net web API project.
+app.MapCarter();
+
+app.Run();
