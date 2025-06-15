caaf7461c6e3ea3e583f0d80a31676be9f9db0d2
Eslam Eid <eeid@itecorp.net>
2025-06-15 17:29:01 +0300

    .

--- eshop-microservices/Services/Ordering/Ordering.API/Program.cs
+++ eshop-microservices/Services/Ordering/Ordering.API/Program.cs
@@ -2,6 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container.
 
+//------------------------------
+//Infrastructure - ef core 
+// application - mediatR
+//api- carter - health check.....
+
+//builder.Services
+//    .AddApplicationServices()
+//    .AddInfrastructureServices(builder.Configuration)
+//    .AddApiServices();
 var app = builder.Build();
 
 // Configure the http request pipeline.
