42b3f69beefa1066347f43008c281acefdfcf21b
Eslam Eid <eeid@itecorp.net>
2025-06-16 16:21:35 +0300

    DDD

--- eshop-microservices/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ eshop-microservices/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,14 +1,15 @@
-﻿//using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Application;
 public static class DependencyInjection
 {
-    //public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-    //{
-    //    //services.AddMediatR(cfg => {
-    //    //    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-    //    //});
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        //services.AddMediatR(cfg =>
+        //{
+        //    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        //});
 
-    //    return services;
-    //}
+        return services;
+    }
 }
