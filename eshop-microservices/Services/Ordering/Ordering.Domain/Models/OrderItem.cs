162531b9058cee8160641678c7e14ad17cfd6526
Eslam Eid <eeid@itecorp.net>
2025-06-17 12:17:36 +0300

    .

---
+++ eshop-microservices/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.Domain.Models;
+public class OrderItem : Entity<Guid>
+{
+    public OrderItem(Guid orderId , Guid productId , int quantity , decimal price)
+    {
+        OrderId = orderId;
+        ProductId = productId;
+        Quantity = quantity;
+        Price = price;
+    }
+    public Guid ProductId { get; set; }
+    public Guid OrderId { get; set; }
+    public int Quantity { get; private set; } = default!;
+    public decimal Price { get; private set; } = default!;
+}
