891a20b1ccfbcf89c2eac545219b367ef1ffe0a6
Eslam Eid <eeid@itecorp.net>
2025-06-22 16:54:37 +0300

    .

--- eshop-microservices/Services/Ordering/Ordering.API/Program.cs
+++ eshop-microservices/Services/Ordering/Ordering.API/Program.cs
@@ -1,18 +1,24 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-//add services to the container.
+// Add services to the container.
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
 
-//------------------------------
-//Infrastructure - ef core 
-// application - mediatR
-//api- carter - health check.....
-
-//builder.Services
-//    .AddApplicationServices()
-//    .AddInfrastructureServices(builder.Configuration)
-//    .AddApiServices();
 var app = builder.Build();
 
-// Configure the http request pipeline.
+// Configure the HTTP request pipeline.
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
--- eshop-microservices/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ eshop-microservices/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Ordering.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure.Data;
 public class ApplicationDbContext : DbContext
@@ -17,10 +12,10 @@ public class ApplicationDbContext : DbContext
     public DbSet<Product> Products => Set<Product>();
     public DbSet<Order> Orders => Set<Order>();
     public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         base.OnModelCreating(builder);
     }
 }
-
